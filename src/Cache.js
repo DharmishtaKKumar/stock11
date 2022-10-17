87ab839c57bbaf8d8b8d7c2231fc596f0d8a001d
DharmishtaK <dharmishta@fabzentech.com>
2022-10-17 11:00:21 +0530

    first-commit

--- src/Cache.js
+++ src/Cache.js
@@ -1,16 +1,16 @@
-import React from "react";
-class Cache {
-  isShowMain = false;
+// import React from "react";
+// class Cache {
+//   isShowMain = false;
 
-  getIsSHow(key) {
-    // get the value out for the given key
-    return isShowMain;
-  }
+//   getIsSHow(key) {
+//     // get the value out for the given key
+//     return isShowMain;
+//   }
 
-  setIsShow(key) {
-    // get the value out for the given key
-    isShowMain = key;
-  }
-}
+//   setIsShow(key) {
+//     // get the value out for the given key
+//     isShowMain = key;
+//   }
+// }
 
-module.exports = new Cache();
+// module.exports = new Cache();
